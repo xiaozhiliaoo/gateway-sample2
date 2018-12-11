fe24c62ddf128fb2fe762d8d7225aae35a6a9670
李力 <772654204@qq.com>
2018-12-12 00:25:11 +0800

    Create GatewaySampleApplication.java

---
+++ src/main/java/org/lili/GatewaySampleApplication.java
@@ -0,0 +1,17 @@
+package org.lili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lili
+ * @description ${DESCRIPTION}
+ * @create 2018-12-10 20:09
+ * @since
+ **/
+@SpringBootApplication
+public class GatewaySampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySampleApplication.class, args);
+    }
+}
