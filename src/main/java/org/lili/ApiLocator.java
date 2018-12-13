2878584df1c6a9130aad983f1997cb98614eee08
李力 <772654204@qq.com>
2018-12-13 13:28:46 +0800

    Update ApiLocator.java

--- src/main/java/org/lili/ApiLocator.java
+++ src/main/java/org/lili/ApiLocator.java
@@ -7,11 +7,14 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 
 /**
  * @author lili
  * @description ${DESCRIPTION}
- * @create 2018-12-11 11:49
+ * @create 2018-12-13 13:20
  * @since
  **/
 @EnableAutoConfiguration
@@ -22,21 +25,61 @@ public class ApiLocator {
     @Autowired
     private RequestFilter requestFilter;
 
+    private static final String SERVICE = "/path/**";
+    private static final String URI = "http://127.0.0.1:8009";
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 
-        RouteLocatorBuilder.Builder serviceProvider = builder.
-                routes().route("gateway-sample",
-                    r -> r.readBody(Object.class, requestBody -> {
-                        log.info("requestBody is {}", requestBody);
-                        // 这里不对body做判断处理
-                        return true;
-                }).and().path("/service").
-                        filters(f -> {
-                            f.filter(requestFilter);
-                            return f;
-                        })
-                        .uri("http://127.0.0.1:8009"));
+        /*
+        route1 是get请求，get请求使用readBody会报错
+        route2 是post请求，Content-Type是application/x-www-form-urlencoded
+        route3 是post请求，Content-Type是application/json
+         */
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        RouteLocatorBuilder.Builder serviceProvider = routes
+                .route("route1",
+                        r -> r
+                                .method(HttpMethod.GET)
+                                .and()
+                                .path(SERVICE)
+                                .filters(f -> {
+                                    f.filter(requestFilter);
+                                    return f;
+                                })
+                                .uri(URI))
+                .route("route2",
+                        r -> r
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                                .and()
+                                .method(HttpMethod.POST)
+                                .and()
+                                .readBody(String.class, readBody -> {
+                                    log.info("request method POST, Content-Type is application/x-www-form-urlencoded, body  is:{}", readBody);
+                                    return true;
+                                })
+                                .and()
+                                .path(SERVICE)
+                                .filters(f -> {
+                                    return f;
+                                })
+                                .uri(URI))
+                .route("route3",
+                        r -> r
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .and()
+                                .method(HttpMethod.POST)
+                                .and()
+                                .readBody(Object.class, readBody -> {
+                                    log.info("request method POST, Content-Type is application/json, body  is:{}", readBody);
+                                    return true;
+                                })
+                                .and()
+                                .path(SERVICE)
+                                .filters(f -> {
+                                    return f;
+                                })
+                                .uri(URI));
         RouteLocator routeLocator = serviceProvider.build();
         log.info("custom RouteLocator is loading ... {}", routeLocator);
         return routeLocator;
